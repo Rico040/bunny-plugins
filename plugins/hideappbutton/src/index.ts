94e89afb9cd772c3b018eb5d558246ac030cfd68
Rico040 <93081680+Rico040@users.noreply.github.com>
2024-05-03 17:39:51 +1000

    feat(plugin): `HideAppButton` skid

---
+++ plugins/hideappbutton/src/index.ts
@@ -0,0 +1,19 @@
+import { findByName } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
+import { getAssetIDByName } from "@vendetta/ui/assets";
+import { findInReactTree } from "@vendetta/utils";
+
+const ChatInput = findByName("ChatInput");
+
+let unpatch: () => boolean;
+
+export default {
+    onLoad() {
+        const blockList = ["ic_thread_normal_24px", "AppsIcon"].map(n => getAssetIDByName(n));
+        unpatch = after("render", ChatInput.prototype, (_, ret) => {
+            const input = findInReactTree(ret, t => "forceAnimateButtons" in t.props && t.props.actions);
+            input.props.actions = input.props.actions.filter(a => !blockList.includes(a.source));
+        });
+    },
+    onUnload: unpatch
+};
